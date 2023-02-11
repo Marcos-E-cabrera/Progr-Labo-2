cc9b3bef87a9c202fcaa7ef3b6704fe8bbebbfe0
marcoscabrera <marcoscabrera021@gmail.com>
2023-02-11 19:08:12 -0300

    Done, Unidad 2- Ejercicio: I06, I07 y I08

---
+++ Unidad_02_Ejercicio_06/Unidad_02_Ejercicio_06/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+                    Ejercicio I06 - Calculadora de áreas
+    Consigna
+    Realizar una clase llamada CalculadoraDeArea que posea tres métodos de clase (estáticos) 
+    que realicen el cálculo del área que corresponda:
+
+    public double CalcularAreaCuadrado(double longitudLado) {}
+
+    public double CalcularAreaTriangulo(double base, double altura) {}
+
+    public double CalcularAreaCirculo(double radio) {}
+
+    El ingreso de los datos como la visualización se deberán realizar desde el método Main().
+ */
+
+namespace Unidad_02_Ejercicio_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese La longitud de Cuadrado: ");
+            double longitud = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"El Area del cuadrado es: " + CalculadoraDeArea.CalcularAreaCuadrado(longitud));
+               
+            Console.Write("Ingrese La base del Triangulo: ");
+            double @base = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la Altura del Triangulo: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("El Area del Triangulo es: " + CalculadoraDeArea.CalcularAreaTriangulo(@base, altura));
+
+            Console.Write("Ingrese el Radio del Cirulo: ");
+            double radio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("El Area del Circulo es: " + CalculadoraDeArea.CalcularAreaCirculo(radio));
+
+        }
+    }
+
+    public static class CalculadoraDeArea
+    {
+        public static double CalcularAreaCuadrado(double longitudLado) 
+        {
+            return (longitudLado * longitudLado);
+        }
+
+        public static double CalcularAreaTriangulo(double @base, double altura) 
+        {
+            return (@base * altura) / 2;
+        }
+
+        public static double CalcularAreaCirculo(double radio) 
+        {
+            return (Math.PI * radio * radio);
+        }
+    }
+}
---
+++ Unidad_02_Ejercicio_07/Unidad_02_Ejercicio_07/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+                Ejercicio I07 - Pitágoras estaría orgulloso
+    Consigna
+    Crear una aplicación de consola que pida al usuario ingresar la base y la altura de un triángulo en 
+    centímetros.
+
+    El programa deberá calcular la longitud de la hipotenusa aplicando el teorema de pitágoras y
+    Usar     los métodos Pow y Sqrt de la clase Math para realizar los cálculos.
+
+    Mostrar el resultado en la consola.
+ */
+
+namespace Unidad_02_Ejercicio_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de longitud de hipotenusa");
+            Console.WriteLine("Ingrese la base del triángulo en centímetros:");
+            double baseTriangulo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la altura del triángulo en centímetros:");
+            double alturaTriangulo = double.Parse(Console.ReadLine());
+
+            double hipotenusa = CalcularHipotenusa(baseTriangulo, alturaTriangulo);
+
+            Console.WriteLine($"La longitud de la hipotenusa es de {hipotenusa:0.##} centímetros");
+        }
+
+        static double CalcularHipotenusa(double baseTriangulo, double alturaTriangulo)
+        {
+            return Math.Sqrt(Math.Pow(baseTriangulo, 2) + Math.Pow(alturaTriangulo, 2));
+        }
+    }
+}
