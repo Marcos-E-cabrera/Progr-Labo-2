8eeb5a9a957ad48cab805bfcd4f736266b38c9c4
marcoscabrera <marcoscabrera021@gmail.com>
2023-04-04 15:42:01 -0300

    Clase-03

---
+++ Metodos_clase02/Estudiante.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos_clase02
+{
+    public class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+        private static Random random;
+
+        static Estudiante()
+        {
+            random = new Random();
+        }
+
+        public Estudiante(string apellido, string legajo, string nombre)
+        {
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+
+        public void SetNotaPrimerParcial(int notaPrimerParcial) 
+        {
+            this.notaPrimerParcial = notaPrimerParcial;
+        }
+        public void SetNotaSegundoParcial(int notaSegundoParcial) 
+        {
+            this.notaSegundoParcial = notaSegundoParcial;
+        }
+
+        private float CalcularPromedio()
+        {
+            int nota1;
+            int nota2;
+            float promedio;
+
+            nota1 = this.notaPrimerParcial;
+            nota2 = this.notaSegundoParcial;
+
+            promedio = (float)(nota1 + nota2) / 2;
+
+            return promedio;
+        }
+
+        public float Promedio
+        {
+            get { return CalcularPromedio(); }
+        }
+
+        public double CalcularNotaFinal()
+        {
+            double notaDelFinal = -1;
+
+            if ( this.notaPrimerParcial >= 4 && this.notaSegundoParcial >= 4) 
+            { 
+                notaDelFinal = random.Next(6, 10);
+            }
+            return notaDelFinal;
+        }
+
+        public double NotaFinal
+        {
+            get { return CalcularNotaFinal(); }
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder aux = new StringBuilder();
+
+            aux.AppendLine($"Nombre: {nombre}  Apellido: {apellido}  Legajo: {legajo}");
+            aux.AppendLine($"Nota del primer y segundo parcial: {notaPrimerParcial}, {notaSegundoParcial}");
+            aux.AppendLine($"Promedio: {Promedio}");
+
+            if (NotaFinal == -1)
+            {
+                aux.AppendLine("Nota final: Alumno desaprobado");
+            }
+            else
+            {
+                aux.AppendLine($"Nota final: {NotaFinal}");
+            }
+
+            return aux.ToString();
+        }
+    }
+}
---
+++ Unidad_03_Ejercicio_i03/Program.cs
@@ -0,0 +1,28 @@
+﻿// https://codeutnfra.github.io/programacion_2_laboratorio_2_apuntes/docs/clases/objetos/Ejercicios/I03-el-ejemplo-universal/
+using Metodos_clase02;
+
+namespace Unidad_03_Ejercicio_i03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante estudiante1 = new Estudiante("Cabrera", "D0001", "Marcos");
+            estudiante1.SetNotaPrimerParcial(9);
+            estudiante1.SetNotaSegundoParcial(8);
+            Console.WriteLine(estudiante1.Mostrar());
+
+            Estudiante estudiante2 = new Estudiante("Pando", "D0002", "Luis");
+            estudiante2.SetNotaPrimerParcial(2);
+            estudiante2.SetNotaSegundoParcial(4);
+            Console.WriteLine(estudiante2.Mostrar());
+
+            Estudiante estudiante3 = new Estudiante("Cabral", "D0003", "Elias");
+            estudiante3.SetNotaPrimerParcial(8);
+            estudiante3.SetNotaSegundoParcial(7);
+            Console.WriteLine(estudiante3.Mostrar());
+
+
+        }
+    }
+}
